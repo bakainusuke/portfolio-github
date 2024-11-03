c7bc82e73b14d4db27706a048038434f7a47490c
bakainusuke <nhathuy520835@gmail.com>
2024-11-04 00:07:02 +1100

    complete initialise Navbar and Footer

---
+++ src/fragments/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="mt-auto py-3 bg-info text-white">
+      <div className="container">Loop Agile Now or LAN &copy; 2022</div>
+    </footer>
+  );
+}
+
+export default Footer;
