f7d91bba26a6e2d61e0c805cebfcbf628c6cd0a6
bakainusuke <82948939+bakainusuke@users.noreply.github.com>
2024-11-04 11:44:12 +1100

    Pages (#5)
    * initalise 4 pages
    
    * route home page first
    
    * add base router name
    
    * update public folder
    
    * update 404
    
    * rollback
    
    * update new way

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import logo from './logo.svg'; 
+// import logo from './logo.svg'; 
 import './App.css';
 import {
   BrowserRouter as Router,
-  // Navigate,
-  // Route,
-  // Routes,
+  Navigate,
+  Route,
+  Routes,
 } from "react-router-dom";
 import Navbar from "./fragments/Navbar";
 import Footer from "./fragments/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Minigame from "./pages/Minigame";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/portfolio-github">
         <Navbar />
-          <main className="flex-grow p-4 bg-gray-100">
-          <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        </main>
+          <main className="App-header flex-grow p-4 bg-gray-100">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/minigame" element={<Minigame />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </main>
         <Footer />
       </Router>
     </div>
