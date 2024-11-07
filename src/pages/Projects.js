b3132a01d779f82dc411ba813666e6a5290f7c0c
bakainusuke <nhathuy520835@gmail.com>
2024-11-07 13:59:45 +1100

    intial

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,40 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Projects.css";
+import projectData from "../data/projectData.json"; // Assuming the data is stored in projectData.json
 
-function Projects(props) {
+const Projects = () => {
+  const [visible, setVisible] = useState([]);
+
+  const toggleVisibleClass = (index) => {
+    setVisible((prevVisible) =>
+      prevVisible.includes(index)
+        ? prevVisible.filter((item) => item !== index)
+        : [...prevVisible, index]
+    );
+  };
+
   return (
-    <div className="container-fluid mt-auto text-white  ">
-      <div className="row min-vh-100">
-        <div className="col-md-6 outer-wrap text-center">
-          <div className="project-module module text project-module-text align- js-project-module e2e-site-project-module-text">
-            <div className="rich-text js-text-editable module-text">
-              <div className="title">
-                <strong>Projects</strong>
-              </div>
+    <div className="projects-container">
+      {projectData.map((project, index) => (
+        <div
+          key={index}
+          className={`project-section ${visible.includes(index) ? "visible" : ""}`}
+          onClick={() => toggleVisibleClass(index)}
+        >
+          <div className="project-card">
+            <img
+              className="project-image"
+              src={`./images/${project.image}`} // Assuming images are in the 'images' folder
+              alt={project.title}
+            />
+            <div className="project-info">
+              <h2 className="project-title">{project.title}</h2>
+              <p className="project-brief">{project.brief}</p>
             </div>
           </div>
-          <div className="project-module module text project-module-text align- js-project-module e2e-site-project-module-text">
-            <div className="rich-text js-text-editable module-text">
-              <div>RMIT Student, Bachelor of Computer Science</div>
-              <div>Artificial Intelligence</div>
-              <div>Machine Learning</div>
-              <div>React</div>
-              <div></div>
-            </div>
-          </div>
-          <div className="js-project-module project-module module social_icons project-module-social_icons align-">
-            <div className="module-content module-content-social_icons js-module-content">
-              <div className="social">
-                <ul></ul>
-              </div>
-            </div>
+
+          <div className={`project-description ${visible.includes(index) ? "show" : ""}`}>
+            <p>{project.description}</p>
           </div>
         </div>
-        <div className="col-md-6 ">
-          <img src="Portfolio/Syd.jpg" alt="This is" className="img-fluid"></img>
-        </div>
-      </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Projects;
