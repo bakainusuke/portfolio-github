a113481537f2847a40a681b52f22941c206c82a9
bakainusuke <nhathuy520835@gmail.com>
2024-11-10 20:00:54 +1100

    complete contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Contact.css"; 
 
 function Contact(props) {
   return (
-    <div className="container-fluid mt-auto text-white  ">
+    <div className="container-fluid mt-auto text-white">
       <div className="row min-vh-100">
-        <div class="col-md-6 outer-wrap text-center">
+        <div className="col-md-6 outer-wrap text-center">
           <div className="project-module module text project-module-text align- js-project-module e2e-site-project-module-text">
             <div className="rich-text js-text-editable module-text">
               <div className="title">
@@ -14,15 +15,57 @@ function Contact(props) {
           </div>
           <div className="project-module module text project-module-text align- js-project-module e2e-site-project-module-text">
             <div className="rich-text js-text-editable module-text">
-              <div>Phone number: 0466998566</div>
-              <div>Email: huydo.henry.work@gmail.com</div>
-              <div></div>
+              <div>Feel free to reach out to me via social media or email.</div>
+              <div>Looking forward to connecting!</div>
+              <p>
+                Phone number:{" "}
+                <a href="tel:+61466998566" className="contact-link">
+                  0466 998 566
+                </a>
+              </p>
+              <p>
+                Email:{" "}
+                <a
+                  href="mailto:huydo.henry.work@gmail.com"
+                  className="contact-link"
+                >
+                  huydo.henry.work@gmail.com
+                </a>
+              </p>
             </div>
           </div>
           <div className="js-project-module project-module module social_icons project-module-social_icons align-">
             <div className="module-content module-content-social_icons js-module-content">
               <div className="social">
-                <ul></ul>
+                <ul>
+                  <li>
+                    <a
+                      href="https://www.facebook.com/n.hyun.hwi/"
+                      className="facebook"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="Facebook"
+                    ></a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/n.huyn.hwi"
+                      className="instagram"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="Instagram"
+                    ></a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/huy-do-hwi/"
+                      className="linkedin"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="LinkedIn"
+                    ></a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
